bd300b1a6951c6b9067b602647f2fc681e566a47
wangruyi007 <122979625@qq.com>
2017-04-28 10:07:38 +0800

    框

---
+++ marketinform-node/static/root/marketinform/information/list/_res/js/controller.js
@@ -0,0 +1,58 @@
+/**
+ * Created by ike on 2017/4/13.
+ */
+var app = angular.module('settlementList', ['ng-pagination','toastr']);
+app.controller('settlementListCtrl',function($scope,settlementSer,toastr) {
+   //选择
+    $scope.selectList = function(event){
+        angular.forEach($scope.settlementLists.data,function(obj){
+            obj._selectList = false
+        });
+        event._selectList = true;
+        $scope.idList = event.id;
+        //向父Ctrl传递事件
+        $scope.$emit('changeId', $scope.idList);
+    };
+    //查看更多
+    $scope.moreList = function(event){
+        angular.forEach($scope.settlementLists.data,function(obj){
+            if(event.id!==obj.id){
+                obj._moreList = false
+            }
+        });
+        event._moreList = !event._moreList;
+    };
+
+    function activatePage(page) {
+        var listData = {
+            page:page
+        }
+        settlementSer.listSettlement(listData).then(function(response){
+            if(response.data.code==0){
+                $scope.settlementLists = response.data
+            }else{
+                toastr.error( "请求超时，请联系管理员", '温馨提示');
+            }
+        });
+    }
+    $scope.abili = {
+        itemsCount: 14, //总条数
+        take: 10, //每页显示
+        activatePage: activatePage
+    };
+    settlementSer.countSettlement().then(function(response){
+        if(response.data.code==0){
+            $scope.abili.itemsCount = response.data.data;
+        }else{
+            toastr.error( "请求超时，请联系管理员", '温馨提示');
+        }
+    });
+    //删除
+    $scope.$on('deletedId',function(event,delid){
+        angular.forEach($scope.settlementLists.data,function(obj){
+            if(obj.id == delid){
+                obj._delete = true
+            }
+        })
+    });
+});
