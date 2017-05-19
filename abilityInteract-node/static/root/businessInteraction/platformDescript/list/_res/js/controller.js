b89b8a222722df337e1906da2ecd844dd22e8c75
daixiuhua <1293225334@qq.com>
2017-05-19 18:34:47 +0800

    modified the abilityInteract-node

---
+++ abilityInteract-node/static/root/businessInteraction/platformDescript/list/_res/js/controller.js
@@ -0,0 +1,63 @@
+var app = angular.module('descriptList', ['ng-pagination','toastr']);
+app.controller('descriptListCtrl',function($scope,descriptSer,toastr){
+    $scope.$emit('changeId', null);
+    $scope.teamInfo = {};
+    function activatePage(page) {
+        var listData = {
+            page:page
+        }
+        descriptSer.descriptList(listData).then(function(response){
+            if(response.data.code==0){
+
+                $scope.descriptLists = response.data.data
+            }else{
+                toastr.error( "请求超时，请联系管理员", '温馨提示');
+            }
+        });
+    }
+
+    $scope.selectList = function(event){
+        angular.forEach($scope.descriptLists,function(obj){
+                obj._selectList = false
+        });
+        event._selectList = true;
+        $scope.idListd = event.id;
+        //向父Ctrl传递事件
+        $scope.$emit('changeId', $scope.idListd);
+
+    };
+    //点击更多详细
+    $scope.moreList = function(event){
+        angular.forEach($scope.descriptLists,function(obj){
+            if(event.id!==obj.id){
+                obj._moreList = false
+            }
+        });
+        event._moreList = !event._moreList;
+    };
+
+    $scope.$on('deletedId',function(event,delid){
+        angular.forEach($scope.descriptLists,function(obj){
+            if(obj.id == delid){
+                obj._delete = delid
+            }
+        })
+    });
+
+//分页
+    $scope.custom = {
+        itemsCount: 2, //总条数
+        take: 10, //每页显示
+        activatePage: activatePage
+    };
+
+    descriptSer.countDescript().then(function(response){
+        if(response.data.code==0){
+            $scope.custom.itemsCount = response.data.data;
+        }else{
+            toastr.error( "请求超时，请联系管理员", '温馨提示');
+        }
+    })
+
+});
+
