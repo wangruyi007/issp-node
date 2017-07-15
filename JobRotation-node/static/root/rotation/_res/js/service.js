d4b0db9a13327fd86e2bbe8d809e168c3490d9ea
lucongji <lucongji_aj@163.com>
2017-07-15 17:33:51 +0800

    ‘岗位轮换’

---
+++ JobRotation-node/static/root/rotation/_res/js/service.js
@@ -0,0 +1,13 @@
+var app = angular.module('rotat');
+app.factory('rotatSer', function($http){
+    return {
+    	setPermission : setPermission,
+        navPermission : navPermission
+    };
+    function setPermission(){
+        return $http.get('/siginmanage/setButtonPermission');
+    }
+    function navPermission(){
+        return $http.get('/siginmanage/sonPermission');
+    }
+});
