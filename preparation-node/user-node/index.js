0112ff6c3149c7e493a365caed2d54ef7d6e5c60
wangruyi007 <122979625@qq.com>
2017-07-28 16:49:17 +0800

    d

---
+++ preparation-node/user-node/index.js
@@ -0,0 +1,32 @@
+const app = require('koa')();//koa web应用
+const path = require('path');//路径
+const router = require("koa-router")();//路由中间件
+const session = require('koa-session');//cookie
+const koaBody = require('koa-body');
+const json = require('koa-json');
+const cors = require('koa-cors');
+const staticCache = require('koa-static-cache');
+const config = require(path.resolve('plugins/read-config.js'));
+const routersPath = '/koa/routers/';
+const port = 80;
+
+app.use(cors());//跨域请求,用于与browsesync调试
+app.keys = ['feedback'];//session加密值
+app.use(session(app));//使用cookie
+app.use(koaBody());//必需要路由用之前使用,不然获取不到表单
+router.get('/', function *(next) {//根路由
+    this.redirect('/index');//重写向到登录页面
+    this.status = 301;
+});
+
+//============路由===========
+app.use(require(path.join(__dirname,routersPath,'index.js'))().routes());//列表路由
+app.use(router.routes());
+
+//============静态文件资源===========
+app.use(staticCache(path.join(__dirname, './static'), {
+    maxAge: 365 * 24 * 60 * 60
+}))
+app.listen(port, function () {
+    console.log('koa server listening on port ' + port);
+});
